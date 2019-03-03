78ea652b5cd692fba40c76b561764fb3ceab12c6
Susie Choi <sc477@duke.edu>
2019-03-03 14:26:52 -0500

    calculating correctness

--- scripts.js
+++ scripts.js
@@ -1,13 +1,18 @@
-var allImageNames = ["EManger34.jpg", "JJhappy24.jpg", "MFanger24.jpg", "MOhappy14.jpg", "NRhappy34.jpg", "PEanger14.jpg", "WFhappy14.jpg", "Chappy34.jpg", "EMhappy14.jpg", "JJanger34.jpg", "MOanger24.jpg", "MFhappy24.jpg", "NRanger14.jpg", "PEanger34.jpg", "SWanger24.jpg", "WFhappy34.jpg", "MOhappy14.jpg", "JJhappy24.jpg", "WFanger34.jpg"];
+var ALL_IMAGE_NAMES = ["EManger34.jpg", "JJhappy24.jpg", "MFanger24.jpg", "MOhappy14.jpg", "NRhappy34.jpg", "PEanger14.jpg", "WFhappy14.jpg", "Chappy34.jpg", "EMhappy14.jpg", "JJanger34.jpg", "MOanger24.jpg", "MFhappy24.jpg", "NRanger14.jpg", "PEanger34.jpg", "SWanger24.jpg", "WFhappy34.jpg", "MOhappy14.jpg", "JJhappy24.jpg", "WFanger34.jpg"];
+var HAPPY_CODINGS = [false, true, false, true, true, false, true, true, true, false, false, true, false, false, false, true, true, true, false]
 var allImages = [];
+
 var questionNumber = 0;
-var allUserInput = [];
+// var allUserInput = [];
+var overallCorrect = 0; 
+var happyCorrect = 0;
+var angryCorrect = 0;
 
 function populateImages() {
 	var imagesAsHTML = ""; 
 	var i;
-	for (i=0; i<allImageNames.length; i++) {
-		imagesAsHTML = imagesAsHTML + "<img src='ebt_photos/"+allImageNames[i]+"' style='display: none'>";
+	for (i=0; i<ALL_IMAGE_NAMES.length; i++) {
+		imagesAsHTML = imagesAsHTML + "<img src='ebt_photos/"+ALL_IMAGE_NAMES[i]+"' style='display: none'>";
 	}
 	document.getElementById("imageContainer").innerHTML = imagesAsHTML;
 }
@@ -28,7 +33,12 @@ function displayNextQuestion() {
 }
 
 function calculateScore() {
-
+	var overallFraction = overallCorrect / ALL_IMAGE_NAMES.length;
+	var overallHappy = happyCorrect / ALL_IMAGE_NAMES.length;
+	var overallAngry = angryCorrect / ALL_IMAGE_NAMES.length;
+	console.log(overallFraction);
+	console.log(overallHappy);
+	console.log(overallAngry);
 }
 
 window.onload = function() {
@@ -39,8 +49,15 @@ window.onload = function() {
 }
 
 function buttonPressed(userInput) {
-	allUserInput.push(userInput);
-	console.log(allUserInput);
+	// allUserInput.push(userInput);
+	if (userInput == "Happy" && HAPPY_CODINGS[questionNumber]) {
+		overallCorrect++;
+		happyCorrect++;
+	}
+	else if (userInput == "Angry" && !HAPPY_CODINGS[questionNumber]) {
+		overallCorrect++;
+		angryCorrect++;
+	}
 	questionNumber++;
 	if (questionNumber < allImages.length) {
 		allImages[questionNumber].style.display = "block";
