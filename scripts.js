8197532350910dc6f2cb6dccbe95e4045796290f
Susie Choi <sc477@duke.edu>
2019-03-03 14:12:15 -0500

    autopopulating page with images

--- scripts.js
+++ scripts.js
@@ -1,6 +1,16 @@
+var allImageNames = ["EManger34.jpg", "JJhappy24.jpg", "MFanger24.jpg", "MOhappy14.jpg", "NRhappy34.jpg", "PEanger14.jpg", "WFhappy14.jpg", "Chappy34.jpg", "EMhappy14.jpg", "JJanger34.jpg", "MOanger24.jpg", "MFhappy24.jpg", "NRanger14.jpg", "PEanger34.jpg", "SWanger24.jpg", "WFhappy34.jpg", "MOhappy14.jpg", "JJhappy24.jpg", "WFanger34.jpg"];
 var allImages = [];
 var questionNumber = 0;
 var allUserInput = [];
+
+function populateImages() {
+	var imagesAsHTML = ""; 
+	var i;
+	for (i=0; i<allImageNames.length; i++) {
+		imagesAsHTML = imagesAsHTML + "<img src='ebt_photos/"+allImageNames[i]+"' style='display: none'>";
+	}
+	document.getElementById("imageContainer").innerHTML = imagesAsHTML;
+}
 
 function showButton() {
     document.getElementById("happyButton").style.visibility = "visible";
@@ -17,7 +27,12 @@ function displayNextQuestion() {
 	setTimeout('showButton()', 1000);
 }
 
+function calculateScore() {
+
+}
+
 window.onload = function() {
+	populateImages();
 	allImages = document.getElementsByTagName("img");
     hideButton();
     displayNextQuestion();
@@ -31,6 +46,9 @@ function buttonPressed(userInput) {
 		allImages[questionNumber].style.display = "block";
 		hideButton();
 		displayNextQuestion();
+	}
+	else {
+		calculateScore(); 
 	}
 }
 
